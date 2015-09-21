a1bc0c32209a471ba20508270a2b32996bc400d8
Dennis Hoer <dennis.hoer@pearson.com>
2015-09-20 20:29:17 -0700

    Add metadata

--- metadata.rb
+++ metadata.rb
@@ -6,24 +6,27 @@ description 'Installs/Configures Selenium-Grid'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 
-depends 'selenium'
+supports 'centos'
+supports 'redhat'
+supports 'mac_os_x'
+supports 'ubuntu'
+supports 'windows'
 
+depends 'selenium'
 depends 'firefox', '~> 2.0'
 depends 'chrome', '~> 1.0'
-
 depends 'chromedriver', '~> 1.0'
 depends 'iedriver', '~> 1.0'
 depends 'safaridriver', '~> 1.0'
-
 depends 'xvfb'
 depends 'windows_screenresolution'
-
 depends 'windows', '~> 1.37.0' # 1.38 spams warning about windows_reboot being removed
 depends 'nssm'
-
 depends 'apt'
 depends 'yum'
 depends 'dmg'
-
 depends 'macosx_autologin'
 depends 'macosx_gui_login'
+
+source_url 'https://github.com/dhoer/chef-selenium_grid' if respond_to?(:source_url)
+issues_url 'https://github.com/dhoer/chef-selenium_grid/issues' if respond_to?(:issues_url)
