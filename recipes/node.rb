021d8a1bd22c5a60224135041d309d25c1ec3732
Dennis Hoer <dennis.hoer@pearson.com>
2016-03-07 11:09:48 -0700

    Fix bzip2 issue on CentOS 7

--- recipes/node.rb
+++ recipes/node.rb
@@ -83,7 +83,6 @@ if node['selenium_grid']['opera']['max_instances'] > 0 && platform?('ubuntu', 'w
 end
 
 if node['selenium_grid']['phantomjs']['max_instances'] > 0 && !platform?('mac_os_x', 'windows')
-  package 'bzip2' if platform?('rhel')
   node['selenium_grid']['phantomjs']['max_instances'].times do |i|
     ghostdriver "ghostdriver_node_#{i}" do
       webdriver "#{node['ipaddress']}:#{8910 + i}"
