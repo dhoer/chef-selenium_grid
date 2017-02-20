1c1c900d7b108226369a7c01298dcf49a8973921
Dennis Hoer <dennis.hoer@pearson.com>
2017-02-19 22:42:47 -0700

    Add test cookbook back in

---
+++ test/fixtures/cookbooks/selenium_grid_test/metadata.rb
@@ -0,0 +1,10 @@
+name 'selenium_grid_test'
+maintainer 'Dennis Hoer'
+maintainer_email 'dennis.hoer@gmail.com'
+license 'MIT'
+description 'Tests Selenium'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+
+depends 'selenium_grid'
+depends 'java_se', '~> 8.0'
